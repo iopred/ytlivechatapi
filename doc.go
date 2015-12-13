377dd6634ddef80b682d8094d74e97a582c26b02
Chris Rhodes <iopred@gmail.com>
2015-12-13 01:11:49 -0800

    Updated demo description.

--- doc.go
+++ doc.go
@@ -3,13 +3,13 @@ Go implementation of the YouTube Live Chat API
 
 Expects an authorized *http.Client, golang.org/x/oauth2 is a good package to use.
 
-Send "Hello world!" to the first default stream.
+Send "Hello world!" to the first default stream chat.
     c := ytlivechatapi.NewClient(&http.Client{})
     if response, err := c.ListLiveBroadcasts("default=true"); err == nil {
       c.InsertLiveChatMessage(ytlivechatapi.NewLiveChatMessage(response.Items[0].Snippet.LiveChatId, "Hello world!"))
     }
 
-Polls on a chat room and bans everyone that sends a message for 10 seconds.
+Polls on the first default stream chat bans everyone that sends a message for 10 seconds.
     c := ytlivechatapi.NewClient(&http.Client{})
     if response, err := c.ListLiveBroadcasts("default=true"); err == nil {
       liveChatId := response.Items[0].Snippet.LiveChatId
