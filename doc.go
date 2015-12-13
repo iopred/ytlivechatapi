4524ad091a32cd66317094029ea6bb87b6f9f558
Chris Rhodes <iopred@gmail.com>
2015-12-13 00:50:48 -0800

    Clean up docs.

--- doc.go
+++ doc.go
@@ -3,8 +3,8 @@ The API expects an authorized *http.Client, golang.org/x/oauth2 is a good packag
 
 Send "Hello world!" to the first default stream.
     c := ytlivechatapi.NewClient(authorizedClient)
-		if broadcasts, err := c.ListLiveBroadcasts("default=true"); err == nil {
-			c.InsertLiveChatMessage(ytlivechatapi.NewLiveChatMessage(broadcasts.Items[0].Snippet.LiveChatId, "Hello world!"))
-		}
+    if broadcasts, err := c.ListLiveBroadcasts("default=true"); err == nil {
+      c.InsertLiveChatMessage(ytlivechatapi.NewLiveChatMessage(broadcasts.Items[0].Snippet.LiveChatId, "Hello world!"))
+    }
 */
 package ytlivechatapi
